helpfulrobot/paulschulzscs-silverstripe-gallery

commit 0d2fba38f0dca2f6ca56219579b9277f82c5eec2

- changed requirements call to themedCSS(), so we can overload css styles

code/Gallery.php
@@ -71,7 +71,7 @@ class Gallery_Controller extends Page_Controller {
 		Requirements::javascript('framework/thirdparty/jquery/jquery.min.js');
 		Requirements::javascript('prettyphoto/js/jquery.prettyPhoto.js');
 		Requirements::css('prettyphoto/css/prettyPhoto.css');
-		Requirements::css('gallery/css/gallery.css');
+		Requirements::themedCSS('gallery/css/gallery.css');
 		
 		$prettyPhotoConfigJson = json_encode(Gallery::$prettyPhotoConfig);
 	

code/GalleryHolder.php
@@ -27,7 +27,7 @@ class GalleryHolder_Controller extends Page_Controller {
 	public function init() {
 		parent::init();
 		
-		Requirements::css('gallery/css/gallery.css');
+		Requirements::themedCSS('gallery');
 	}
 
 }

commit 740e365294dd90276be62c4a53aced64ea76a58c

-Fixed path to themedCss(...), bug introduced in 0d2fba38

code/Gallery.php
@@ -71,7 +71,7 @@ class Gallery_Controller extends Page_Controller {
 		Requirements::javascript('framework/thirdparty/jquery/jquery.min.js');
 		Requirements::javascript('prettyphoto/js/jquery.prettyPhoto.js');
 		Requirements::css('prettyphoto/css/prettyPhoto.css');
-		Requirements::themedCSS('gallery/css/gallery.css');
+		Requirements::themedCSS('gallery');
 		
 		$prettyPhotoConfigJson = json_encode(Gallery::$prettyPhotoConfig);
 	